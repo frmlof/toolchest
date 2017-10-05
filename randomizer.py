50b87dfc690dadb73f8ee34b6447726c453f5f9b
Nazim Aliyev <aliyevn@amazon.com>
2017-10-05 16:40:41 +0000

    Adding randomized

---
+++ randomizer.py
@@ -0,0 +1,13 @@
+#!env python
+
+import sys
+import string
+import random
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        generate_string = lambda n: ''.join([random.choice(string.lowercase + string.uppercase) for i in xrange(n)])
+        my_string = generate_string(int(sys.argv[1]))
+        print(my_string)
+    else:
+        sys.exit('Please provide length in integers of string your want to generate')
